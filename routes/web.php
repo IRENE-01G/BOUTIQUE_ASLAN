f6e41fd6ec0fbfab1c883b2959fc5e46e6d3be8d
ebossro <bossrokodjoviemmanuel031@gmail.com>
2026-01-05 18:44:12 +0000

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Accueil');
 });
+
+Route::get('/services', function () {
+    return view('Services');
+});
+
+Route::get('/apropos', function () {
+    return view('Apropos');
+});
